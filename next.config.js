fa0293d4819f399a6f94d4698961a74e754051ab
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-02 23:49:49 +0530

    backend url added
    Signed-off-by: AbhishekUnthread <abhishek.k@unthread.world>

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    BASE_URL: 'http://localhost:8000/api/v1',
-    ENV: 'development',
+    BASE_URL: 'https://cryptocalc-backend-production.up.railway.app/api/v1',
+    ENV: 'production',
     ACCESS_TOKEN_EXPIRATION_TIME: '3600000',
     TIME_BUFFER: '300000',
     EMAIL: 'test@gmail.com',
